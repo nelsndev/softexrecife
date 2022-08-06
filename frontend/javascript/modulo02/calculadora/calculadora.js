c0ec91a33ee26a8336ef15de888034e027c745fb
Nelson Henrique <nhsneto@gmail.com>
2022-08-06 19:22:33 -0300

    Adiciona calculadora.js

---
+++ frontend/javascript/modulo02/calculadora/calculadora.js
@@ -0,0 +1,38 @@
+const prompt = require("prompt-sync")();
+
+const valor1 = parseInt(prompt("Valor1: "));
+const valor2 = parseInt(prompt("Valor2: "));
+const operador = prompt("Operador(+ - * /): ");
+
+let resultado = 0;
+
+switch (operador) {
+  case "+":
+    resultado = valor1 + valor2;
+    break;
+  case "-":
+    resultado = valor1 - valor2;
+    break;
+  case "*":
+    resultado = valor1 * valor2;
+    break;
+  case "/":
+    resultado = divisao(valor1, valor2);
+    break;
+  default:
+    resultado = "Operador inválido.";
+    break;
+}
+
+function divisao(primeiroValor, segundoValor) {
+  if (segundoValor === 0) return "Operação inválida. [divisão por 0]";
+
+  if (primeiroValor % segundoValor === 0) {
+    return primeiroValor / segundoValor;
+  } else {
+    let resultado = parseInt(primeiroValor / segundoValor);
+    return `Resultado: ${resultado}; Sobra: ${primeiroValor % segundoValor}`;
+  }
+}
+
+console.log(resultado);
