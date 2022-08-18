f97ee9f9e221f26078dceab75921c38075bd1595
Nelson Henrique <nhsneto@gmail.com>
2022-08-17 22:55:13 -0300

    Remove codigo nao utilizado

--- frontend/javascript/modulo03/atividade-express/app.js
+++ frontend/javascript/modulo03/atividade-express/app.js
@@ -7,8 +7,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/")
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log("Server started at http://localhost:" + PORT));
