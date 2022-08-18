6c1e09e806fcc9f1c4b4e6cf6bf3cdd0b30154dc
Nelson Henrique <nhsneto@gmail.com>
2022-08-17 22:48:47 -0300

    Adiciona atividade-express

---
+++ frontend/javascript/modulo03/atividade-express/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/")
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Server started at http://localhost:" + PORT));
