fc50d22d95fa10058ac434007745e28cbb509436
Nelson Henrique <nhsneto@gmail.com>
2022-10-21 12:38:34 -0300

    Remove botao de dentro paragrafo

--- frontend/react/meu-aplicativo/src/Contador.js
+++ frontend/react/meu-aplicativo/src/Contador.js
@@ -4,15 +4,17 @@ import "./Contador.css";
 function Contador() {
   let [contador, setContador] = useState(0);
   return (
-    <p className="Contador-paragrafo">
-      Contagem: <span className="Contador-contagem">{contador}</span>
+    <>
+      <p className="Contador-paragrafo">
+        Contagem: <span className="Contador-contagem">{contador}</span>
+      </p>
       <button
         className="Contador-botao"
         onClick={() => setContador(++contador)}
       >
         Contar
       </button>
-    </p>
+    </>
   );
 }
 
